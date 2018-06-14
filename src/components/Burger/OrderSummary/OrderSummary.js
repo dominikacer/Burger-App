bfc5a768f86d997d1be2c691442d740c67151dbf
Dominik M <dominik.michalik@gmail.com>
2018-06-14 21:08:37 +0200

    change structure of folders

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
@@ -32,7 +32,7 @@ class OrderSummary extends Component {
             <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
                 CANCEL
             </Button>
-            <Button btnType="Success" clicked={this.props.purchaseContinue}>
+            <Button btnType="Success" clicked={this.props.purchaseContinueHandler}>
                 CONTINUE
             </Button>
         </Aux>
