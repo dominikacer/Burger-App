a9832b74be136c94f6c8ff2898334c4df39ada57
Dominik M <dominik.michalik@gmail.com>
2018-06-14 20:56:23 +0200

    crazy improve app

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    
-    const ingridientSummary = Object.keys(props.ingridients)
+class OrderSummary extends Component {
+    // this could be a functional component, doesn't have to be a class
+    // this is how it is because it is easier to debug app
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+
+    render(){
+
+        const ingridientSummary = Object.keys(this.props.ingridients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingridients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingridients[igKey]}
                 </li>);
         });
-
-    return (
-        <Aux>
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients: </p>
             <ul>
                 {ingridientSummary}
             </ul>
-            <p><strong>Total Price : {props.price.toFixed(2)} </strong></p>
+            <p><strong>Total Price : {this.props.price.toFixed(2)} </strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
                 CANCEL
             </Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>
+            <Button btnType="Success" clicked={this.props.purchaseContinue}>
                 CONTINUE
             </Button>
         </Aux>
-    );
-};
+        );
 
-export default orderSummary;
+    }
+}
+
+export default OrderSummary;
