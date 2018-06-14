e1124116cdffe0a86f62979461ec5fc8045eb061
Dominik M <dominik.michalik@gmail.com>
2018-06-14 19:59:52 +0200

    Side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,10 +5,12 @@ import Aux from '../../hoc/Aux';
 // import styles
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Aux>
         <Toolbar />
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
